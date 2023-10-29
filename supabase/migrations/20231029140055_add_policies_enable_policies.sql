c4773373874037863aeb58f3a28917090c06978c
Victor B <bellemin.victor@gmail.com>
2023-10-29 11:46:25 -0400

    update for trigger on user add

--- supabase/migrations/20231029140055_add_policies_enable_policies.sql
+++ supabase/migrations/20231029140055_add_policies_enable_policies.sql
@@ -1,3 +1,7 @@
 alter table "public"."Partogramme" enable row level security;
 
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
 
