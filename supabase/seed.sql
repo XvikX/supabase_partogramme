604d6fd3f3525c63a8fd731929bec27bc6298c82
Victor B <bellemin.victor@gmail.com>
2023-11-05 19:22:10 -0500

    add seed values

---
+++ supabase/seed.sql
@@ -0,0 +1,18 @@
+WITH credentials(mail, pass, uuid) AS (
+  -- PUT YOUR EMAILS AND PASSWORDS HERE.
+  SELECT * FROM (VALUES 
+                        ('doc@email.io', 'doc', '108f618b-5a0b-4f9f-ac40-3642cb0e7fbb'), 
+                        ('nurse1@email.io', 'nurse1', 'f0319ed1-5f6a-4461-90c3-ae78a7a4a14a'),
+                        ('nurse2@email.io', 'nurse2', 'e64f4caa-e8b3-4f6a-a2ab-b731d7ce91fc')
+                        ) AS users
+),
+create_user AS (
+  INSERT INTO auth.users (id, instance_id, ROLE, aud, email, raw_app_meta_data, raw_user_meta_data, is_super_admin, encrypted_password, created_at, updated_at, last_sign_in_at, email_confirmed_at, confirmation_sent_at, confirmation_token, recovery_token, email_change_token_new, email_change)
+    SELECT CAST(uuid as uuid), '00000000-0000-0000-0000-000000000000', 'authenticated', 'authenticated', mail, '{"provider":"email","providers":["email"]}', '{}', FALSE, crypt(pass, gen_salt('bf')), NOW(), NOW(), NOW(), NOW(), NOW(), '', '', '', '' FROM credentials
+  RETURNING id
+)
+INSERT INTO auth.identities (id, provider, user_id, identity_data, last_sign_in_at, created_at, updated_at)
+  SELECT id, 'email', id, json_build_object('sub', id), NOW(), NOW(), NOW() FROM create_user;
+
+-- set user roles
+SELECT set_claim('108f618b-5a0b-4f9f-ac40-3642cb0e7fbb', 'userrole', '"doctor"');
